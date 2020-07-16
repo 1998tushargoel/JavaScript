e58435d863ee1dd249416c7074ba82db6188fded
1998tushargoel <1998tushargoel@gmail.com>
2020-07-16 16:54:52 +0530

    AI created

--- Tic_Tac_Toe/script.js
+++ Tic_Tac_Toe/script.js
@@ -27,7 +27,12 @@ function startGame(){
 
 function turnClick(square) {
     // console.log(square.target.id);
-    turn(square.target.id, huPlayer);
+    if (typeof origBoard[square.target.id] == 'number'){
+        turn(square.target.id, huPlayer);
+        if(!checkTie()){
+            turn(bestSpot(), aiPlayer);
+        }
+    }   
 }
 
 function turn(squareId, player){
@@ -58,4 +63,30 @@ function gameOver(gameWon){
     for( var i=0 ; i < cells.length ; i++){
         cells[i].removeEventListener('click', turnClick, false)
     }
+    declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose!");
+}
+
+function declareWinner(who) {
+    document.querySelector(".endgame").style.display = "block";
+    document.querySelector(".endgame .text").innerText = who;
+}
+
+function bestSpot() {
+    return emptySqauers()[0];
+}
+
+function emptySqauers() {
+    return origBoard.filter(s => typeof s == 'number');
+}
+
+function checkTie() {
+    if(emptySqauers().length == 0){
+        for ( var i=0 ; i< cells.length; i++){
+            cells[i].style.backgroundColor = "green";
+            cells[i].removeEventListener('click', turnClick, false);
+        }
+        declareWinner("Tie Game!")
+        return true;
+    }
+    return false;
 }
